09003dd90b15106e98523c593707be959b468eb4
Arash Kaveh <arashkaveh2006@gmail.com>
2025-08-20 12:47:41 +0330

    created first app

--- website/views.py
+++ website/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse , JsonResponse
 
-# Create your views here.
+
+def index_view(request):
+    return HttpResponse('<h1>PROJECT-ORION HOME PAGE</h1>')
+
+def about_view(request):
+    return HttpResponse('<h1>PROJECT-ORION ABOUT PAGE</h1>')
+
+def contact_view(request):
+    return HttpResponse('<h1>PROJECT-ORION CONTACT PAGE</h1>')
+
